d5a12b3e2865d95aab8bcd15a599d7d5f59be1ef
Adreet <adreetchakraborty091999@gmail.com>
2025-08-28 21:06:33 +0530

    edited

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -6,5 +6,6 @@ package com.google;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("All the best for placement !Get placed soon and keep hustling,keep energy high ,keep learning how to code efficinetly and keep growing ");
     }
 }
